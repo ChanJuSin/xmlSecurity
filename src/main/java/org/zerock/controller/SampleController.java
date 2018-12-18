0c2e6a4aa6bcfc5a76bab6b0de733f844b8f239d
ChanJuSin <ckswn1259@naver.com>
2018-12-18 20:46:31 +0900

    xml 설정기반 스프링 시큐리티

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,44 @@
+package org.zerock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*")
+public class SampleController {
+
+	private static final Logger log = LoggerFactory.getLogger(SampleController.class);
+	
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		log.info("logined member");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		log.info("admin only");
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+	@GetMapping("/annoMember") 
+	public void doMember2() {
+		
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		
+	}
+	
+}
